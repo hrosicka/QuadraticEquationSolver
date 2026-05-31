106383304f2928bcf55878ff205615db5aa6de9a
Hanka <hrosicka@post.cz>
2026-05-31 11:07:04 +0200

    feat(math): handle edge cases when coefficients are zero

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -5,18 +5,13 @@ class QuadraticEquation:
     """ 
     Calculation of quadratic equation including complex roots
     """
-    
 
     def __init__(self, a, b, c):
         """
         Constructor
-
         a, b, c - coefficients - integers
-
         ax^2 - quadratic term
-
         bx - linear term
-
         c - constant term
         """
         self.a = a
@@ -31,13 +26,9 @@ class QuadraticEquation:
     def discriminant(self):
         """
         D = b^2 - 4*a*c
-
         3 possibilities:
-
         D < 0 - 2 complex roots
-
         D = 0 
-
         D > 0 - 2 roots
         """
         D = pow(self.b,2) - 4*self.a*self.c
@@ -47,59 +38,51 @@ class QuadraticEquation:
     def equation_type(self, D):
         """
         D - discriminant
-
-        return equation typy as a string
+        return equation type as a string
         """
-
-        if D < 0:
+        if self.a == 0:
+            return "Not a quadratic equation (a = 0)."
+        elif self.b == 0 and self.c == 0:
+            return "Equation has a double root at zero (b = 0, c = 0)."
+        elif D < 0:
             return "The roots are complex."
-
         elif D == 0:
             return "Equation has 1 root - the two roots are real and equal to each other."
-
         else:
             return "Equation has 2 roots - real and unequal."
 
 
-
     def solve(self):
         """
-        Solution of qaudratic equation
-
-        It can be 3 types of solution:
-
-        D < 0 - when dicriminant is negative, equation has two complex solutions
-
-        D = 0 - when dicriminant is zero, equation has just one solution
-        
-        D > 0 - when dicriminant is positive, equation has two real solutions
-
-        Roots:
-
-        x1 = (-b+sqrt(D))/(2*a)
-
-        x2 = (-b-sqrt(D))/(2*a)
-
-        Vertex calculation
+        Solution of quadratic equation
         """
+        # --- EDGE CASE 1: If a = 0, it is not a quadratic equation ---
+        if self.a == 0:
+            raise ValueError("Coefficient 'a' cannot be zero in a quadratic equation.")
+
+        # --- EDGE CASE 2: If b = 0 and c = 0 => x^2 = 0 => x = 0 ---
+        if self.b == 0 and self.c == 0:
+            self.x1 = self.x2 = 0.0
+            self.vertex_x = 0.0
+            self.vertex_y = 0.0
+            return  # Early return to skip unnecessary calculations
+
+        # --- Standard calculation ---
         D = self.discriminant()
 
         if D == 0:
-            self.x1 = self.x2 = round(-self.b / (2 * self.a),3)
+            self.x1 = self.x2 = round(-self.b / (2 * self.a), 3)
             
         elif D > 0:
-            self.x1 = round((-self.b + math.sqrt(D)) / (2 * self.a),3)
-            self.x2 = round((-self.b - math.sqrt(D)) / (2 * self.a),3)
+            self.x1 = round((-self.b + math.sqrt(D)) / (2 * self.a), 3)
+            self.x2 = round((-self.b - math.sqrt(D)) / (2 * self.a), 3)
 
-           
         else:
-            self.x1 = (-self.b + cmath.sqrt(D))/ (2 * self.a)
-            self.x2 = (-self.b - cmath.sqrt(D))/ (2 * self.a)
-            self.x1 = complex(round(self.x1.real,3),round(self.x1.imag,3))
-            self.x2 = complex(round(self.x2.real,3),round(self.x2.imag,3))
+            self.x1 = (-self.b + cmath.sqrt(D)) / (2 * self.a)
+            self.x2 = (-self.b - cmath.sqrt(D)) / (2 * self.a)
+            self.x1 = complex(round(self.x1.real, 3), round(self.x1.imag, 3))
+            self.x2 = complex(round(self.x2.real, 3), round(self.x2.imag, 3))
 
-
+        # Vertex calculation for standard cases
         self.vertex_x = -self.b / (2 * self.a)
-        self.vertex_y = self.a*self.vertex_x*self.vertex_x + self.b*self.vertex_x + self.c
-
-            
+        self.vertex_y = self.a * self.vertex_x * self.vertex_x + self.b * self.vertex_x + self.c
